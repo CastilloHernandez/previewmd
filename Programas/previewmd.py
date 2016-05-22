8befa71b329355131012e295c2459f4bbfb3c3cf
Emmanuel Castillo <emmanuel156@gmail.com>
2016-05-21 22:44:33 -0500

    Se agrega parametro para procesar todos los directorios o solo los seleccionados.

--- Programas/previewmd.py
+++ Programas/previewmd.py
@@ -2,6 +2,7 @@ import os
 import mimetypes
 import hashlib
 import urllib2
+import argparse
 
 def get_redirected_url(url):
 	opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
@@ -11,7 +12,6 @@ def get_redirected_url(url):
 def BuscarImagenes(dir):
 	r=[]
 	for root, dirs, files in os.walk(dir):
-		#print root
 		for file in files:	
 			if str(mimetypes.guess_type(file)[0]).split('/')[0] == 'image':
 				r.append(str(os.path.join(root,file)).split('trunk')[1])
@@ -19,86 +19,96 @@ def BuscarImagenes(dir):
 def BuscarReadmes(dir):
 	r=[]
 	for root, dirs, files in os.walk(dir):
-		#print root
 		for file in files:
-			#print file
 			if file == 'README.md':
 				r.append(root)
 	return sorted(r)
 
+parser = argparse.ArgumentParser(prog='previewmd')
+parser.add_argument('-branch',default='trunk')
+parser.add_argument('directorio',nargs='*')
+opt = parser.parse_args()
 
-for d in BuscarReadmes('.'):
-	#print d
-	c=[]
-	o=''
-	UrlAvatar=''
-	HashOriginal=''
-	HashModificado=''
-	Seccion=''
-	NecesitaLineaEnBlanco=False
-	AgregarContenido=False
-	ImagenesAgregadas=False
-	with open(os.path.join(d,'README.md'),'r') as f:
-		o=f.read().replace('\r\n','\n')
-		HashOriginal=hashlib.md5(o).hexdigest()
-		for l in o.splitlines():
-			if l.startswith('##'):
-				Seccion=l[3:]
-			if l.startswith('## Contenido'):
-				AgregarContenido=True
-				break
-			if not l.startswith('---'):
-				if not l.startswith('!['):
-					if l.startswith('* '):
-						if l[2:].count(' ') == 0:
-							try:
+directorios=[]
+if len(opt.directorio):
+	directorios=opt.directorio
+else:
+	directorios.append('.')
+
+for directorio in directorios:
+	for d in BuscarReadmes(directorio):
+		c=[]
+		o=''
+		UrlAvatar=''
+		HashOriginal=''
+		HashModificado=''
+		Seccion=''
+		NecesitaLineaEnBlanco=False
+		AgregarContenido=False
+		ImagenesAgregadas=False
+		with open(os.path.join(d,'README.md'),'r') as f:
+			o=f.read().replace('\r\n','\n')
+			HashOriginal=hashlib.md5(o).hexdigest()
+			for l in o.splitlines():
+				if l.startswith('## '):
+					Seccion=l[3:]
+				if l.startswith('## Contenido'):
+					AgregarContenido=True
+					break
+				if not l.startswith('---'):
+					if not l.startswith('!['):
+						if l.startswith('* '):
+							if l[2:].count(' ') == 0:
 								if Seccion == 'Autores':
-									UrlAvatar=get_redirected_url('http://www.github.com/' + l[2:] + '.png')
-									c.append('* <a href="http://www.github.com/' + l[2:] + '">' + l[2:] + '</a> <img src="' + UrlAvatar + '&s=50">\n')	
+									try:
+										print 'Buscando avatar de ' + l[2:]
+										UrlAvatar=get_redirected_url('http://www.github.com/' + l[2:] + '.png')
+										print 'Avatar encontrado ' + UrlAvatar
+										c.append('* <a href="http://www.github.com/' + l[2:] + '">' + l[2:] + '</a> <img src="' + UrlAvatar + '" height="32" width="32">\n')	
+									except:
+										print 'Avatar no encontrado'
+										c.append(l + '\n')
 								else:
 									c.append(l + '\n')									
-							except:
+							else:
 								c.append(l + '\n')
 						else:
 							c.append(l + '\n')
-					else:
-						c.append(l + '\n')
-		while len(c[-1])<=1:
-			c.pop()
+			while len(c[-1])<=1:
+				c.pop()
+			if len(c[-1])>1:
+				NecesitaLineaEnBlanco=True
+		if AgregarContenido:
+			if NecesitaLineaEnBlanco:
+				NecesitaLineaEnBlanco=False
+				c.append('\n')
+			c.append('## Contenido\n')
+			for r in BuscarReadmes(d):
+				if not r == d:
+					with open(os.path.join(r,'README.md'),'r') as f:
+						for l in f.read().splitlines():
+							if l.startswith('# '):
+								c.append('* ' + l[2:] + '\n')
+							if l.startswith('## '):
+								c.append('\t* ' + l[3:] + '\n')
+							if l.startswith('* '):
+								c.append('\t\t* ' + l[2:] + '\n')
 		if len(c[-1])>1:
 			NecesitaLineaEnBlanco=True
-	if AgregarContenido:
-		if NecesitaLineaEnBlanco:
-			NecesitaLineaEnBlanco=False
-			c.append('\n')
-		c.append('## Contenido\n')
-		for r in BuscarReadmes(d):
-			if not r == d:
-				with open(os.path.join(r,'README.md'),'r') as f:
-					for l in f.read().splitlines():
-						if l.startswith('# '):
-							c.append('* ' + l[2:] + '\n')
-						if l.startswith('## '):
-							c.append('\t* ' + l[3:] + '\n')
-						if l.startswith('* '):
-							c.append('\t\t* ' + l[2:] + '\n')
-	if len(c[-1])>1:
-		NecesitaLineaEnBlanco=True
-	for i in BuscarImagenes(d):
-		ImagenesAgregadas=True
-		if NecesitaLineaEnBlanco:
-			NecesitaLineaEnBlanco=False
-			c.append('\n')
-		c.append('---\n')
-		c.append('![' + os.path.basename(i) + '](' + i.replace(chr(92),chr(47)) + ')\n')
-	if ImagenesAgregadas:
-		c.append('---\n')
-	HashModificado=hashlib.md5(''.join(c)).hexdigest()
-	if HashOriginal==HashModificado:
-		print 'Sin cambios: ' + os.path.join(d,'README.md')
-	else:
-		print 'Modificado: ' + os.path.join(d,'README.md')
-		with open(os.path.join(d,'README.md'),'wb') as f:
-			f.writelines(c)
+		for i in BuscarImagenes(d):
+			ImagenesAgregadas=True
+			if NecesitaLineaEnBlanco:
+				NecesitaLineaEnBlanco=False
+				c.append('\n')
+			c.append('---\n')
+			c.append('![' + os.path.basename(i) + '](' + i.replace(chr(92),chr(47)) + ')\n')
+		if ImagenesAgregadas:
+			c.append('---\n')
+		HashModificado=hashlib.md5(''.join(c)).hexdigest()
+		if HashOriginal==HashModificado:
+			print 'Sin cambios: ' + os.path.join(d,'README.md')
+		else:
+			print 'Modificado: ' + os.path.join(d,'README.md')
+			with open(os.path.join(d,'README.md'),'wb') as f:
+				f.writelines(c)
 
-	#print BuscarImagenes(d)
